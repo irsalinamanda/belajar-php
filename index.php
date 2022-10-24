d6b9d03b0f282ae38079c8f70570702c7596175f
Irsalina Amanda Aisyah <irsalinaisyah14@gmail.com>
2022-10-24 10:57:39 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Hallo!!<br>";
+
+$nama = "Irsalina";
+$umur = 21;
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur Tahun.";                                
+
+$namaAdik = "Luna";
+$umurAdik = 8;
+echo "<br>Nama adik saya $namaAdik, usia adik saya $umurAdik Tahun.";
+
+$selisih = $umur-$umurAdik;
+echo "<br>Selisih usia saya dan adik saya adalah $selisih Tahun.";
+?>
