755a0b3fa89fb9fc4fa7949d8360ae056e32876d
Irsalina Amanda Aisyah <irsalinaisyah14@gmail.com>
2022-11-24 08:28:38 +0700

    tambah data mysql

--- index.php
+++ index.php
@@ -1,15 +1,53 @@
 <?php
+//1. buat koneksi dg mysql
+$con = mysqli_connect("localhost","root","","fakultas");
 
-echo "Hallo!!<br>";
+//2. Cek koneksi dg mysql
+if(mysqli_connect_errno()){
+    echo "Koneksi gagal". mysqli_connect_error();
+} else{
+    echo "Koneksi berhasil";
+}
 
-$nama = "Irsalina";
-$umur = 21;
-echo "Nama saya <strong>$nama</strong>, saya berusia $umur Tahun.";                                
+//3. membaca data dari table mysql;
+$query = "SELECT * FROM mahasiswa";
 
-$namaAdik = "Luna";
-$umurAdik = 8;
-echo "<br>Nama adik saya $namaAdik, usia adik saya $umurAdik Tahun.";
+//4. tampilkan data, dengan menjalankan sql query
+$result = mysqli_query($con,$query);
+$mahasiswa = [];
+if ($result){
+    // tampilkan data satu per satu
+    while($row = mysqli_fetch_assoc($result)){
+        $mahasiswa[] = $row;
+        }
+    mysqli_free_result($result);
+}
 
-$selisih = $umur-$umurAdik;
-echo "<br>Selisih usia saya dan adik saya adalah $selisih Tahun.";
+//5. tutup koneksi mysql
+mysqli_close($con);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <table border="1" style="width:100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value): ?>
+        <tr>
+            <td><?php echo $value["nim"]; ?></td>
+            <td><?php echo $value["nama"]; ?> </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
